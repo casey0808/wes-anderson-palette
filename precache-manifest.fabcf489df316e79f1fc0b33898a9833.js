a53fec3f082e7cd526135e9087a5d18b375854ca
casey0808 <casey_c0808@outlook.com>
2020-03-01 13:50:14 +0800

    Updates

---
+++ precache-manifest.fabcf489df316e79f1fc0b33898a9833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a4dc918c2f0530ca4e10cbde85b8742",
+    "url": "/wes-anderson-palette/index.html"
+  },
+  {
+    "revision": "d345480bae0f7d27ed25",
+    "url": "/wes-anderson-palette/static/css/main.961e13fd.chunk.css"
+  },
+  {
+    "revision": "984c158e3d84612e168f",
+    "url": "/wes-anderson-palette/static/js/2.a3919ef2.chunk.js"
+  },
+  {
+    "revision": "c8194f9b7a1b3f7465580c3787025109",
+    "url": "/wes-anderson-palette/static/js/2.a3919ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d345480bae0f7d27ed25",
+    "url": "/wes-anderson-palette/static/js/main.64ed9098.chunk.js"
+  },
+  {
+    "revision": "97e984d5ef071553b6e5",
+    "url": "/wes-anderson-palette/static/js/runtime-main.d370c477.js"
+  },
+  {
+    "revision": "afc1bf561fafffe73a867cf1a3541e7b",
+    "url": "/wes-anderson-palette/static/media/PD.afc1bf56.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wes-anderson-palette/precache-manifest.fabcf489df316e79f1fc0b33898a9833.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wes-anderson-palette/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
